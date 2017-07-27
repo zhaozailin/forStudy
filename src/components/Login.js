31bc814a53e68e43409a86e536f73d19bae220a3
zhaozailin <zhaozl@servyou.com.cn>
2017-07-27 20:48:34 +0800

    js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,12 @@
 /**
  * Created by zhaozailin on 2017/7/27.
  */
+import React from 'react';
+import '../styles/login.css';
+
+class Login extends React.Component {
+    render() {
+        return (<h1>login</h1>);
+    }
+}
+export default Login;
