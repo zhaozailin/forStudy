217e9169fd3f4bd04a9c331486b86467515c86f8
zhaozailin <zhaozl@servyou.com.cn>
2017-07-27 21:48:03 +0800

    js

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -237,7 +237,7 @@ module.exports = {
                 autoprefixer({
                   browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4'],
                 }),
-                // pxtorem({ rootValue: 100, propWhiteList: [] })
+                pxtorem({ rootValue: 100, propWhiteList: [] })
               ],
             },
           },
--- src/components/Login.js
+++ src/components/Login.js
@@ -10,11 +10,11 @@ class Login extends React.Component {
         return (<div>
             <div className="login-title">智学，让学习更简单</div>
             <div className="login-input login-user">
-                <i className="iconfont">&#xe64a;</i>
+                <div><i className="iconfont">&#xe64a;</i></div>
                 <input type="text" placeholder="用户名"/>
             </div>
             <div className="login-input login-password">
-                <i className="iconfont">&#xe66b;</i>
+                <div><i className="iconfont">&#xe66b;</i></div>
                 <input type="password" placeholder="请输入密码"/>
             </div>
         </div>);
