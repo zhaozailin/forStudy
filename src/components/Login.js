38c7ac246c811f5ee02d60b4d2d579812eee5572
zhaozailin <zhaozl@servyou.com.cn>
2017-07-27 23:17:51 +0800

    js

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,21 +3,25 @@
  */
 import React from 'react';
 import '../styles/login.css';
-// import {  } from 'antd-mobile';
+import {Button} from 'antd-mobile';
+import '../../node_modules/antd-mobile/dist/antd-mobile.css';
 
 class Login extends React.Component {
     render() {
-        return (<div>
-            <div className="login-title">智学，让学习更简单</div>
-            <div className="login-input login-user">
-                <div><i className="iconfont">&#xe64a;</i></div>
-                <input type="text" placeholder="用户名"/>
+        return (
+            <div>
+                <div className="login-title">智学，让学习更简单</div>
+                <div className="login-input login-user">
+                    <div><i className="iconfont">&#xe64a;</i></div>
+                    <input type="text" placeholder="用户名"/>
+                </div>
+                <div className="login-input login-password">
+                    <div><i className="iconfont">&#xe66b;</i></div>
+                    <input type="password" placeholder="请输入密码"/>
+                </div>
+                <Button className="btn login-btn" activeClassName="login-btn-active">登录</Button>
             </div>
-            <div className="login-input login-password">
-                <div><i className="iconfont">&#xe66b;</i></div>
-                <input type="password" placeholder="请输入密码"/>
-            </div>
-        </div>);
+        );
     }
 }
 export default Login;
