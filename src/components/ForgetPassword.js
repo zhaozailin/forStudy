666d66e653a98f45ef82e27b963ba422e7e3b18c
zhangbin <zhangbin@servyou.com.cn>
2017-07-28 15:24:41 +0800

    js

--- src/components/ForgetPassword.js
+++ src/components/ForgetPassword.js
@@ -5,16 +5,29 @@
 import React from 'react';
 import { Button} from 'antd-mobile';
 import '../styles/password.css';
-
 class ForgetPassword extends React.Component {
-    render() {
 
+    render() {
         return (
             <div>
-                <div className="password-input">
-                    <input />
+
+
+                <div className="password-title">为学网-找回密码</div>
+                <div className="password-top">
+                    <div className="password-step step1 password-step-active">填写账号</div>
+                    <div className="password-step step2">验证身份</div>
+                    <div className="password-step step3">设置新密码</div>
                 </div>
-                <Button className="btn password-btn" type="primary">下一步</Button>
+
+                <div>
+                    <div className="password-input">
+                        <input type="text" placeholder="请输入账号或者绑定的手机号"/>
+                    </div>
+                    <div className="password-input password-verify">
+                        <input type="text" placeholder="请输入验证码"/>
+                    </div>
+                    <Button className="btn password-btn" type="primary">下一步</Button>
+                </div>
              </div>);
 
     }
